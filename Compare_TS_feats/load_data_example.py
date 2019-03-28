727a4d48a289f540987c9f23d85b96e4337edc4c
Eamonn Kennedy <eamonn_kennedy@brown.edu>
2019-03-28 17:45:31 -0400

    Code for chemical identification added

--- Compare_TS_feats/load_data_example.py
+++ Compare_TS_feats/load_data_example.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
 
-This is a temporary script file.
+Basic read in code for time series features
+-Eamonn
+
 """
 import os
 import numpy as np
@@ -24,44 +25,107 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import load_digits
 from sklearn.manifold import MDS
 from sklearn.neighbors import KNeighborsClassifier  
+import platform
 
-wdir            = r"C:\Users\Eamonn\Documents\GitHub\RC_utilities\Compare_TS_feats\example_feat_data"
-fname_EtOH = r'feature_struct.mat'
-fname_Ace = r'feature_struct2.mat'
+#a = platform.uname()
+#if a[1] == 'XPS15': # settings for graphs on 4K XPS15 only
+#    plt.style.use(['seaborn-paper']) # ,'dark_background'])
+#    matplotlib.rcParams['figure.figsize']   = (22, 22)
+#    matplotlib.rcParams['axes.titlesize']   = 30
+#    matplotlib.rcParams['axes.labelsize']   = 30
+#    matplotlib.rcParams['lines.linewidth']  = 2
+#    matplotlib.rcParams['lines.markersize'] = 20
+#    matplotlib.rcParams['xtick.labelsize']  = 30
+#    matplotlib.rcParams['ytick.labelsize']  = 30
 
+#------------------------------------------------------------------------------
+#%%
+
+wdir = r"C:\Users\Eamonn\Documents\GitHub\RC_utilities\Compare_TS_feats\example_feat_data"
+fname = r'chem_ts_feat'
+
+# Load the data
 os.chdir(wdir)
+TSF_data = sio.loadmat(fname + '.mat')
+label_dict = TSF_data[fname]['labels'][0][0]
+feat_dict = TSF_data[fname]['feat_mat'][0,0]
+chem_names = TSF_data[fname]['chem_names'][0,0]
+num_chems = np.shape(chem_names)[0]
+num_feats = np.shape(feat_dict[0][chem_names[0][0][0]][0])[0]
 
-data = sio.loadmat(fname_EtOH)
-EtOH_data = data['feature_struct']
-EtOH_labels = EtOH_data['labels'][0,0]
-EtOH_mat = EtOH_data['lv_norm'][0,0]
+# Core information, pattern labels, features, chemicals for dictionaries
+labels = {}
+feat_mat = {}
+chem_name = {}
 
-data = sio.loadmat(fname_Ace)
-Ace_data = data['feature_struct2']
-Ace_labels = Ace_data['labels'][0,0]
-Ace_mat = Ace_data['lv_norm'][0,0]
+# And some ancillary information
+num_traces = np.zeros(num_chems)
+feat_max_arr = np.zeros((num_feats,num_chems))
+feat_min_arr = np.zeros((num_feats,num_chems))
 
-num_feats = np.shape(Ace_mat)[1]
-num_traces = np.shape(Ace_mat)[0]
-num_messages = len(np.unique(Ace_labels))
+# Get the labels and feature matrix for every chemical
+for chem_num in range(num_chems):
 
-acc = np.zeros(num_messages)
-accuracy_matrix = np.zeros((num_feats,num_messages))
-for feat_num in range(num_feats):
+    chem_name[chem_num] = chem_names[chem_num][0][0]
+    feat_mat[chem_num] = feat_dict[chem_name[chem_num]][0,0]
+    feat_mat[chem_num][np.isnan(feat_mat[chem_num])] = 0 # remove nan entries
+    labels[chem_num] = label_dict[chem_name[chem_num]][0][0]
+    num_traces[chem_num] = np.shape(feat_mat[chem_num])[1]
+    feat_max_arr[:,chem_num] = np.max(feat_mat[chem_num],1)
+    feat_min_arr[:,chem_num] = np.min(feat_mat[chem_num],1)
     
-    #plt.plot(Ace_labels,Ace_mat[:,feat_num],'.')
-    #plt.plot(EtOH_labels,EtOH_mat[:,feat_num],'.')
-    #plt.xlabel('message ID')
-    #plt.ylabel('Feature values')
-    #plt.show
+# Normalize data from 0 - 1 
+abs_max = np.nanmax(feat_max_arr,1) # max and min over all chemicals by feature
+abs_min = np.nanmin(feat_min_arr,1)
+range_feat = abs_max - abs_min + 10**-6
+feat_mat_norm = feat_mat
+for chem_num in range(num_chems):
     
-    for trace_num in range(num_messages):
+    for f in range(num_feats):
+        all_trace_vals = feat_mat[chem_num][f,:]
+        all_trace_vals = (all_trace_vals - abs_min[f]) / range_feat[f]
+        feat_mat_norm[chem_num][f,:] = all_trace_vals    
+        
+#-----------------------------------------------------------------------------
+#%%
+
+#plt.loglog(np.median(Ace_mat,1),np.median(EtOH_mat,1),'.')
+#plt.loglog(np.mean(Ace_mat,1),np.mean(EtOH_mat,1),'.')
+#plt.loglog(np.max(Ace_mat,1),np.max(EtOH_mat,1),'.')
+#plt.loglog(np.min(Ace_mat,1),np.min(EtOH_mat,1),'.')
+#plt.xlim((10**-5,10**5));
+#plt.ylim((10**-2,10**1))
+
+num_msgs = len(np.unique(labels[c]))
+acc = np.zeros(num_msgs)
+accuracy_matrix = np.zeros((num_feats,num_msgs))
+plot_feat_num = 2
+for f in range(num_feats):
     
-        v1 = Ace_mat[np.where(Ace_labels==trace_num+1)[0],feat_num]
-        v2 = EtOH_mat[np.where(EtOH_labels==trace_num+1)[0],feat_num]
+    if f<plot_feat_num:   
+        for c in range(num_chems):        
+            plt.scatter(labels[c],feat_mat_norm[c][f,:])   
+        plt.title(f)
+        plt.xlabel('message ID')
+        plt.ylabel('Feature values')
+        # plt.legend('EtOH','Ace')
+        plt.show()
+    
+    # Seperate out data by every pattern sent,, determine chemical discrimination
+    for trace_num in range(num_msgs):
+        
+        # Seperate out features by chemical, plot a 6x4 grid for 24 traces
+        v1 = feat_mat_norm[0][f,np.where(labels[0]==trace_num+1)[0]]
+        v2 = feat_mat_norm[1][f,np.where(labels[1]==trace_num+1)[0]]
         num_obs = len(v1)+len(v2)
         vals_feat = (np.concatenate((v1,v2),0))
         label_feat = np.concatenate((np.zeros(len(v1)),np.ones(len(v2))),0)
+        if f<plot_feat_num: 
+            plt.subplot(6,4,trace_num+1)
+            plt.scatter(range(len(vals_feat)),vals_feat,c=label_feat,vmin=-.5,vmax=1.5)
+            plt.title(trace_num) 
+            plt.ylim((0,1))
+            plt.yticks([])
         
         # Iterate 1NN over all points and test against the true labels
         label_1nn = np.zeros(num_obs)
@@ -69,15 +133,41 @@ for feat_num in range(num_feats):
             distances = np.abs(vals_feat - vals_feat[i])
             distances[i] = np.max(distances) + 1 # rule out the value itself
             label_1nn[i] = label_feat[np.where(distances==np.min(distances))[0][0]]
-#         print(trace_num,np.sum(1*(label_feat==label_1nn))/num_obs)    
+        # print(trace_num,np.sum(1*(label_feat==label_1nn))/num_obs)    
         acc[trace_num] = np.sum(1*(label_feat==label_1nn))/num_obs
+       
+    plt.show()
         
-    accuracy_matrix[feat_num,:] = acc
+    accuracy_matrix[f,:] = acc
     
 plt.stem(np.mean(accuracy_matrix,1))
 plt.show()
 plt.stem(np.mean(accuracy_matrix,0))
 plt.show()  
+
+plt.semilogy(1 - np.mean(accuracy_matrix[:,:],1),'.');plt.ylim((.0001,.3))
+for i in range(7):
+    plt.plot((i*219,i*219),(.0001,.3),'k')
+plt.xlabel('Feature index, V = 1...219,P=220,...')
+plt.ylabel('Error rate on chemical identification')
+plt.xlim((0,num_feats))
+plt.title('Single feature error identification rates averaged over all patterns')
+plt.show() 
+
+num_vars = 6
+threshold = np.array([.9,.95,.98,.99,.995])
+feats_found = np.zeros((len(threshold),num_vars))
+tick = 0
+for j in threshold:
+    for i in range(num_vars):
+        feats_found[tick,i] = np.sum(np.mean(accuracy_matrix[i*219:(i+1)*219,:],1)>=j)
+    tick = tick+1
+
+plt.plot(threshold,feats_found,'o')
+plt.ylabel('# Features classifying with accuracy>X')
+plt.xlabel('Accuracy')
+plt.show() 
+
      
 #plt.scatter(range(len(vals_feat)),vals_feat,c=label_feat)
 #plt.plot(label_1nn)
